227065cd3091522d6fc4042aefdc3b3a70fbe52f
Mr-w-h-y <114408432+Mr-w-h-y@users.noreply.github.com>
2022-09-27 06:37:03 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,11 +6,7 @@ echo "Downloading v2ray-plugin ${V2RAY_VERSION}"
 wget --quiet -O- "https://github.com/shadowsocks/v2ray-plugin/releases/download/${V2RAY_VERSION}/v2ray-plugin-linux-amd64-${V2RAY_VERSION}.tar.gz" | tar zx
 
 echo "Downloading go-shadowsocks2 ${GOSS_VERSION}"
-
 wget --quiet -O- "https://github.com/shadowsocks/go-shadowsocks2/releases/download/${GOSS_VERSION}/shadowsocks2-linux.gz" | gunzip -c > go-shadowsocks2
-
-
-
 
 mv v2ray-plugin_linux_amd64 v2ray-plugin
 chmod +x start.sh v2ray-plugin go-shadowsocks2
